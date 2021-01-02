69d0ea33fcd4e9eb892f4af51afd9dcfecca04cf
MASTERAMARJEET <51752782+MASTERAMARJEET@users.noreply.github.com>
2021-01-02 19:11:01 +0530

    Cursor parallax effect added (still no animations)

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+// Parallax.js code
+var starBg = document.getElementById('starBg');
+var parallaxInstance = new Parallax(starBg);
+
+var jupiter = document.getElementById('jupiter');
+var jupiterInstance = new Parallax(jupiter);
