f00386ec1cd8f3750b0850a412a73404b3ae65ea
MASTERAMARJEET <51752782+MASTERAMARJEET@users.noreply.github.com>
2021-01-02 22:02:48 +0530

    Retraction animation added in the navbar

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,23 @@ var parallaxInstance = new Parallax(starBg);
 
 var jupiter = document.getElementById('jupiter');
 var jupiterInstance = new Parallax(jupiter);
+
+
+// Scroll effect on navbar
+let lastScrollY = 0;
+
+window.addEventListener("scroll", scrollFunction)
+
+function scrollFunction() {
+
+	var currentScrollY = document.documentElement.scrollTop;
+
+	if ( currentScrollY > lastScrollY) {
+		document.querySelector(".nav-container").style.top = "-80px";
+  } else {
+		document.querySelector(".nav-container").style.top = "0px";
+	}
+
+	lastScrollY = currentScrollY;
+
+}
