2ad8d15b9186d687bee36b63b3708e4466ddf75a
MASTERAMARJEET <51752782+MASTERAMARJEET@users.noreply.github.com>
2021-01-02 22:57:26 +0530

    Bounce animation added to arrow

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,16 @@ var jupiterInstance = new Parallax(jupiter);
 
 
 // Scroll effect on navbar
+// and controlling arrow bounce according to scroll
 let lastScrollY = 0;
 
 window.addEventListener("scroll", scrollFunction)
 
+const arrow = document.querySelector(".arrow");
+
 function scrollFunction() {
 
+	// controlling navbar
 	var currentScrollY = document.documentElement.scrollTop;
 
 	if ( currentScrollY > lastScrollY) {
@@ -22,5 +26,13 @@ function scrollFunction() {
 	}
 
 	lastScrollY = currentScrollY;
+
+	// controlling arrow bounce
+	if (document.documentElement.scrollTop > 0){
+		arrow.classList.remove("arrow-bounce")
+	}
+	else{
+		arrow.classList.add("arrow-bounce")
+	}
 
 }
